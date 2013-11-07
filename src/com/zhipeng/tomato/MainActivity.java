0d244c6d800d11081a3adcd3e9701bffe670952d
jesusjzp <jesusjzp@gmail.com>
2013-11-07 09:41:34 +0100

    integrer ActionbarSherlock simple theme

--- src/com/zhipeng/tomato/MainActivity.java
+++ src/com/zhipeng/tomato/MainActivity.java
@@ -1,13 +1,18 @@
 package com.zhipeng.tomato;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 import android.support.v4.view.ViewPager;
 
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.viewpagerindicator.PageIndicator;
 import com.viewpagerindicator.TitlePageIndicator;
 
-public class MainActivity extends BaseSampleActivity {
+public class MainActivity extends SherlockFragmentActivity {
+	
+	TestFragmentAdapter mAdapter;
+    ViewPager mPager;
+    PageIndicator mIndicator;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
