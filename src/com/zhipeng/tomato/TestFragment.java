42930360b99e3fc70a9fafd3904fc1df876a34f4
jesusjzp <jesusjzp@gmail.com>
2013-11-09 16:15:46 +0100

    add progresswheel

--- src/com/zhipeng/tomato/TestFragment.java
+++ src/com/zhipeng/tomato/TestFragment.java
@@ -1,5 +1,6 @@
 package com.zhipeng.tomato;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.Gravity;
@@ -12,6 +13,8 @@ import android.widget.TextView;
 
 public final class TestFragment extends Fragment {
     private static final String KEY_CONTENT = "TestFragment:Content";
+    private ProgressWheel pw;
+    private TextView text;
 
     public static TestFragment newInstance(String content) {
         TestFragment fragment = new TestFragment();
@@ -37,17 +40,36 @@ public final class TestFragment extends Fragment {
         }
     }
 
-    @Override
+    @SuppressLint("ResourceAsColor")
+	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        TextView text = new TextView(getActivity());
+        
+        pw = new ProgressWheel(getActivity(), null);
+        LayoutParams params = new LayoutParams(
+        		(int) (getResources().getDisplayMetrics().density * 250), 
+        		(int) (getResources().getDisplayMetrics().density * 250));
+        params.gravity = Gravity.CENTER;
+        pw.setLayoutParams(params);
+        pw.setText(getString(R.string.initTime));
+        pw.setTextColor(R.color.textColor);
+        pw.setTextSize((int) (50 * getResources().getDisplayMetrics().density));
+        pw.setRimColor(R.color.rimColor);
+        pw.setRimWidth(10);
+        pw.setBarColor(R.color.barColor);
+        pw.setBarWidth(20);
+        pw.setBarLength(60);
+        
+        text = new TextView(getActivity());
         text.setGravity(Gravity.CENTER);
-        text.setText(mContent);
-        text.setTextSize(20 * getResources().getDisplayMetrics().density);
+        text.setText(getString(R.string.give_up));
+        text.setTextSize(10 * getResources().getDisplayMetrics().density);
         text.setPadding(20, 20, 20, 20);
-
+        
         LinearLayout layout = new LinearLayout(getActivity());
-        layout.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+        layout.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        layout.setOrientation(1);
         layout.setGravity(Gravity.CENTER);
+        layout.addView(pw);
         layout.addView(text);
 
         return layout;
